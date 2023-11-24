b5b3a966426028774148ece8a1a79db63de414ba
saad25011 <jd070099@gmail.com>
2023-11-24 12:00:59 -0600

    java_questions

---
+++ app.js
@@ -0,0 +1,173 @@
+// #JS literal notation
+let student_name = [];
+// JS object notation
+let suture_student_name = {};
+
+// Declare and initialize a strings array.
+
+let names = ["ali", "maaz", "saad", "owais"];
+console.log(names)
+
+// Declare and initialize a numbers array.
+
+let age = [16, 18, 20, 22, 23]
+console.log(age)
+
+// Declare and initialize a boolean array.
+
+let weight = [1.2, 1.4, 1.9, 2.9]
+console.log(weight)
+
+// Declare and initialize a mixed array.
+
+let mixed_array = ["saad", "Artificial intelligence", 22, 60.5]
+
+console.log(mixed_array)
+
+// Declare and Initialize an array and store available
+// education qualifications in Pakistan (e.g. SSC, HSC, BCS,
+// BS, BCOM, MS, M. Phil., PhD).
+
+let education = ["SSC", "HSC", "BCS", "BS" , "BCOM", "MS", "M_PHILL", "PHD"]
+console.log(education)
+
+
+// QUESTION NO 8
+
+let students = ["Saad", "Maaz", "Ali"];
+
+let marks = [320, 420, 390]
+
+console.log("Score of " + students[0] + " is " + marks[0] + ". Percentage " + (marks[0]/500)*100 + "%")
+console.log("Score of " + students[1] + " is " + marks[1] + ". Percentage " + (marks[1]/500)*100 + "%")
+console.log("Score of " + students[2] + " is " + marks[2] + ". Percentage " + (marks[2]/500)*100 + "%")
+
+
+// Question 9
+// Add volor in beginning 
+
+// let color_names = ["red", "yellow", "dark_red", "pink", "baby_pink"]
+
+// var new_one = prompt("Enter Your Favourate Color")
+// let new_color = color_names.unshift(new_one)
+
+// console.log(color_names)
+
+// Add color in End
+// let color_names = ["red", "yellow", "dark_red", "pink", "baby_pink"]
+// var end_one = prompt("Enter Color in End")
+// let end_color = color_names.push(end_one)
+
+// console.log(color_names)
+
+// Add two elements in the beginning
+// let color_names = ["red", "yellow", "dark_red", "pink", "baby_pink"]
+
+// console.log(color_names)
+
+// var first = prompt("Enter First color ")
+// var second = prompt("Enter Second color ")
+
+// let new_array = color_names.splice(0,2, first, second)
+
+// console.log(color_names)
+
+// Remove the first Element in the Array
+
+// let color_names = ["red", "yellow", "dark_red", "pink", "baby_pink"]
+// console.log(color_names)
+
+// let delete_one  = color_names.shift()
+
+// console.log(color_names)
+// Remove the last Element in the Array
+
+// let color_names = ["red", "yellow", "dark_red", "pink", "baby_pink"]
+// console.log(color_names)
+
+// let delete_one  = color_names.pop()
+
+// console.log(color_names)
+// Add color on particular index
+
+// let color_names = ["red", "yellow", "dark_red", "pink", "baby_pink"]
+
+// console.log(color_names)
+
+// var first = parseInt(prompt("Enter Index number "))
+
+// var first1 = prompt("Enter First color ")
+
+// let new_array = color_names.splice(first,1, first1)
+
+// console.log(color_names)
+
+// Question 10
+
+// let score = [320, 380, 280, 150]
+
+// console.log(score)
+
+// console.log(score.sort())
+
+// Question 11
+
+// var array_one = ["This" , " is ", " my ", " cat"]
+
+// console.log(array_one.join(" "))
+
+// Question 12
+
+// var Array_one = ["saad", "keyboard", "mouse", "cpu"]
+
+// let new_array = Array_one.shift()
+
+
+// console.log(new_array)
+
+// let new_array1 = Array_one.shift()
+
+
+// console.log(new_array1)
+
+// let new_array2 = Array_one.shift()
+
+
+// console.log(new_array2)
+
+// let new_array3 = Array_one.shift()
+
+
+// console.log(new_array3)
+
+// QUESTION 13
+
+// var Array_one = ["saad", "keyboard", "mouse", "cpu"]
+
+// let new_array = Array_one.pop()
+
+
+// console.log(new_array)
+
+// let new_array1 = Array_one.pop()
+
+
+// console.log(new_array1)
+
+// let new_array2 = Array_one.pop()
+
+
+// console.log(new_array2)
+
+// let new_array3 = Array_one.pop()
+
+
+// console.log(new_array3)
+
+
+// QUESTION 14
+
+
+var Array_one = ["Nokia", " Apple", " Sumsung", " oppo"]
+
+document.write(Array_one)
